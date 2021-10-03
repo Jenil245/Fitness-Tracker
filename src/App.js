77da658dc5cd469cb232c1d3691ce2b086ac8bc7
Jenil Patel <jenil2452000@gmail.com>
2021-10-03 18:21:00 +0530

    Fixed UI issue

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Card><FitnessActivities /></Card>
+        <FitnessActivities />
       </header>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,9 @@
 import "./Card.css";
 
 function Card(props) {
-    const classes = 'card' + props.className;
+    console.log(props.className)
+    console.log(props.children)
+    const classes = 'card ' + props.className;
 
     return (
         <div className={classes}>{props.children}</div>
