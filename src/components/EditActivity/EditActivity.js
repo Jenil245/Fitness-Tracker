f128ec57981b0fd71cf95cd74f0971772001024f
Jenil Patel <jenil2452000@gmail.com>
2021-10-14 22:34:52 +0530

    minor changes in EditActivity

--- src/components/EditActivity/EditActivity.js
+++ src/components/EditActivity/EditActivity.js
@@ -1,13 +1,16 @@
+import { Fragment } from 'react';
 import EditActivityForm from './EditActivityForm';
 import './EditActivity.css'
 
 
 function EditActivity (props) {
     return (
+        <Fragment>
         <div className="edit-activity">
-           <div className="header"> Please Enter New Values</div>
+           <div className="header"> Please Edit the values & hit the Update Activity button</div>
             <EditActivityForm presentActivityData={props.presentActivityData} onEditActivity={props.onEditActivity} onChangeStatus={props.onChangeStatus} />
         </div>
+        </Fragment>
     )
 }
 
